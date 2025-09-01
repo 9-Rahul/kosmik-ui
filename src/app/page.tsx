2bdb18837cbc2d6724e1650871ac5c5817f766fb
Rahul Thapliyal <9.rahul02@gmail.com>
2025-09-02 04:52:56 +0530

    Banner Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Header from "@/my_components/Pages/Header";
 import "../app/Page.css";
+import Header from "@/my_components/Pages/Header";
+import Banner from "@/my_components/Pages/Banner";
 export default function Home() {
   return (
     <>
       <div className="main">
         <Header />
+        <Banner />
       </div>
     </>
   );
---
+++ src/my_components/Pages/Banner.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "../Styles/Banner.css";
+import { Archivo } from "next/font/google"; // ✅ Font import
+
+const archivo = Archivo({
+  weight: ["700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+function Banner() {
+  return (
+    <section className="banner-outer">
+      <div className="banner-inner">
+        <h1 className={archivo.className}>
+          The <span>only moodboarding </span> platform <span>you need</span>
+        </h1>
+
+        <div className="banner-btns">
+          <button className="orange-btn-banner ">Try now for free</button>
+          <button className="download-btn">Download Kosmik</button>
+        </div>
+
+        <div className="banner-vid-container">
+          <video
+            src="https://framerusercontent.com/assets/74XgTPwBa2QGz1aTJkMEkwwHM.mp4"
+            autoPlay
+            muted
+            loop
+            playsInline
+          ></video>
+        </div>
+
+        <div className="banner-btm-text">
+          <h3>
+            Out of hand download folder ? <br />
+            Too many Pinterest boards ?
+          </h3>
+          <h3>
+            Sooooo many tabs ? <br />
+            Messy desktop ?
+          </h3>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Banner;
